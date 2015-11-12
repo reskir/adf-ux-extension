c36489bfdf2b135af5970770dfd6dc26413be161
k.abaskin <k.abaskin@adform.com>
2015-11-12 17:46:59 +0300

    insert on top of head

--- content_script.dev.js
+++ content_script.dev.js
@@ -1,10 +1,10 @@
 (function(){
-	
+
 	if(!window.uxExtension) return;
-	
+
 	var linksToRemove = uxExtension.linksToRemove || [];
 	var linksToAdd = uxExtension.linksToAdd || [];
-	
+
 	var links = document.getElementsByTagName("link");
 	for(var i=0;i<linksToRemove.length;i++){
 		for(var n=0;n<links.length;n++){
@@ -13,16 +13,16 @@
 			}
 		}
 	}
-	
+
 	var timestamp = (new Date().getTime());
 	var head = document.head || document.getElementsByTagName('head')[0];
 	for(var i=0;i<linksToAdd.length;i++){
-		
+
 		var href = linksToAdd[i]; href += href.indexOf("?") === -1 ? "?" : "&"; href += "ord="+timestamp;
 		var style = document.createElement("link");
 		style.rel = "stylesheet";
 		style.type = "text/css";
 		style.href = href;
-		head.appendChild(style);
+		head.insertBefore(style, head.firstChild);
 	}
 })();
--- content_script.js
+++ content_script.js
@@ -11,8 +11,7 @@ if(response.data == undefined || response.data == ""){ return; }
 		contentScripts += 'window.uxExtension.linksToAdd = ["'+response.data.sessionAddCss.join('","')+'"];';
 
 		/*njected js */
-		contentScripts += '!function(){if(window.uxExtension){for(var e=uxExtension.linksToRemove||[],n=uxExtension.linksToAdd||[],t=document.getElementsByTagName("link"),o=0;o<e.length;o++)for(var d=0;d<t.length;d++)t[d].href&&-1!==t[d].href.indexOf(e[o])&&t[d].parentNode.removeChild(t[d]);for(var r=new Date().getTime(),a=document.head||document.getElementsByTagName("head")[0],o=0;o<n.length;o++){var i=n[o];i+=-1===i.indexOf("?")?"?":"&",i+="ord="+r;var l=document.createElement("link");l.rel="stylesheet",l.type="text/css",l.href=i,a.appendChild(l)}}}();';
-
+		contentScripts += '!function(){if(window.uxExtension){for(var e=uxExtension.linksToRemove||[],n=uxExtension.linksToAdd||[],t=document.getElementsByTagName("link"),o=0;o<e.length;o++)for(var r=0;r<t.length;r++)t[r].href&&-1!==t[r].href.indexOf(e[o])&&t[r].parentNode.removeChild(t[r]);for(var i=(new Date).getTime(),d=document.head||document.getElementsByTagName("head")[0],o=0;o<n.length;o++){var a=n[o];a+=-1===a.indexOf("?")?"?":"&",a+="ord="+i;var l=document.createElement("link");l.rel="stylesheet",l.type="text/css",l.href=a,d.insertBefore(l,d.firstChild)}}}();'
 		if(contentScripts !== ""){
 			var actualCode = contentScripts;
 			var script = document.createElement("script"); script.textContent = actualCode; (document.head||document.documentElement).appendChild(script); script.parentNode.removeChild(script);
